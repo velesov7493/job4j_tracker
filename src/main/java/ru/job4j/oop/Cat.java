07c32332d65bf23852727faea31f83e01f4f71ba
velesov7493 <velesov7493@yandex.ru>
2021-05-06 04:19:56 +0000

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.println(name + " съел " +food);
+    }
+
+    public void eat(String meat) {
+        food = meat;
+    }
+
+    public void giveNick(String nick) {
+        name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Кормим Гава:");
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("котлета");
+        gav.show();
+        System.out.println("Кормим Черныша:");
+        Cat black = new Cat();
+        black.giveNick("Черныш");
+        black.eat("рыба");
+        black.show();
+    }
+}
