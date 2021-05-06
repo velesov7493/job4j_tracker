bb4aa77ff30aa9b68d93e776fff12b6ff3bdacd5
velesov7493 <velesov7493@yandex.ru>
2021-05-06 13:41:45 +0000

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {}
+
+    public Error(boolean aActive, int aStatus, String aMessage) {
+        active = aActive;
+        status = aStatus;
+        message = aMessage;
+    }
+
+    public void printInfo() {
+        System.out.println("Активна: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        Error paramsError1 = new Error(true, 3, "Ошибка с заполненными полями 1");
+        Error paramsError2 = new Error(false, -11, "Ошибка с заполненными полями 2");
+        defaultError.printInfo();
+        paramsError1.printInfo();
+        paramsError2.printInfo();
+    }
+}
