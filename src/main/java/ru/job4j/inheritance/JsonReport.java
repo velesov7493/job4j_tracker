a3f5ae66d7f0797606ba672bb6c3c5c47f6af859
velesov7493 <velesov7493@yandex.ru>
2021-05-07 16:49:15 +0000

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JsonReport.java
+++ src/main/java/ru/job4j/inheritance/JsonReport.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class JsonReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
         return
                 "{" + System.lineSeparator() +
