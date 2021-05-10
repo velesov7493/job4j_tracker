bb25c230ba39008a1d80ead9669c97304b70dace
velesov7493 <velesov7493@yandex.ru>
2021-05-10 05:02:46 +0000

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,34 @@ public class StartUITest {
         String expected = "Fix PC";
         assertEquals(expected, created.getName());
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String expected = "replaced item";
+        String[] answers = {
+                String.valueOf(item.getId()),
+                expected
+        };
+        StartUI ui = new StartUI(new StubInput(answers), tracker);
+        ui.cmdEditItem();
+        Item replaced = tracker.findById(item.getId());
+        assertEquals(expected, replaced.getName());
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId())
+        };
+        StartUI ui = new StartUI(new StubInput(answers), tracker);
+        ui.cmdDeleteItem();
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
+    }
 }
