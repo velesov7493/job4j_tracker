790c9ae233f1e1e41e0f9f9bd5831a27247e6d3e
velesov7493 <velesov7493@yandex.ru>
2021-05-11 14:22:46 +0000

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #1206]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -3,9 +3,11 @@ package ru.job4j.ex;
 public class Fact {
 
     public static int calc(int n) {
-        int result = n > 0 ? 1 : 0;
+        int result = 1;
         if (n < 0) {
             throw new IllegalArgumentException("N должно быть неотрицательным!");
+        } else if (n == 0) {
+            return 0;
         }
         for (int index = 1; index <= n; index++) {
             result *= index;
