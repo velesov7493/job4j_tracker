a82c1cde64e981faa5678b0c39099dafefbaa260
velesov7493 <velesov7493@yandex.ru>
2021-05-11 14:39:27 +0000

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #1214]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -3,13 +3,14 @@ package ru.job4j.ex;
 public class FactRec {
 
     public static int calc(int n) {
-        int result = 0;
-        if (n == 1) {
-            result = 1;
-        } else if (n > 1) {
-            result = calc(n - 1) * n;
+        if (n < 0) {
+            throw new IllegalArgumentException("N должно быть неотрицательным!");
         }
-        return result;
+        return switch (n) {
+            case 0 -> 0;
+            case 1 -> 1;
+            default -> calc(n - 1) * n;
+        };
     }
 
     public static void main(String[] args) {
