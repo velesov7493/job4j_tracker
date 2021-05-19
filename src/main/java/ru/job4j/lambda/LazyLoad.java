700e1de42a3202f0c2f90706572daba344efbc94
velesov7493 <velesov7493@yandex.ru>
2021-05-19 14:04:35 +0000

    2.5. Ленивая загрузка [#249211]

---
+++ src/main/java/ru/job4j/lambda/LazyLoad.java
@@ -0,0 +1,22 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LazyLoad {
+
+    public static void main(String[] args) {
+        String[] names = {
+                "Elena",
+                "Alexandr",
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("Выполнение сравнения");
+            return left.length() - right.length();
+        };
+        Arrays.sort(names, lengthCmp);
+        for (String name: names) {
+            System.out.println(name);
+        }
+    }
+}
