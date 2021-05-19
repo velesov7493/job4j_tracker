d2beff37d9421443dc1845f129278b7c71053bfc
velesov7493 <velesov7493@yandex.ru>
2021-05-19 10:12:08 +0000

    2. Отсортировать департаменты [#34352 #1609]

--- src/main/java/ru/job4j/collection/departments/DepartmentsDescComp.java
+++ src/main/java/ru/job4j/collection/departments/DepartmentsDescComp.java
@@ -4,31 +4,12 @@ import java.util.Comparator;
 
 public class DepartmentsDescComp implements Comparator<String> {
 
-    /**
-     * Разделение последовательности на голову и хвост
-     * Часть codeSequence до символа "/" - голова, а после - хвост
-     * @param codeSequence разделяемая последовательность
-     * @return Массив из 2-х строк. [0] - голова, [1] - хвост.
-     */
-    private String[] headTail(String codeSequence) {
-        String[] result = new String[2];
-        int i = codeSequence.indexOf('/');
-        if (i < 0) {
-            result[0] = codeSequence;
-            result[1] = "";
-            return result;
-        }
-        result[0] = codeSequence.substring(0, i);
-        result[1] = codeSequence.substring(i + 1);
-        return result;
-    }
-
     @Override
     public int compare(String left, String right) {
-        String[] chunks1 = headTail(left);
-        String[] chunks2 = headTail(right);
+        String[] chunks1 = left.split("/", 2);
+        String[] chunks2 = right.split("/", 2);
         int result = chunks2[0].compareTo(chunks1[0]);
-        if (result != 0) {
+        if (result != 0 || chunks1.length == 1 || chunks2.length == 1) {
             return result;
         }
         result = chunks1[1].compareTo(chunks2[1]);
