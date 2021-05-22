3bd9dfdaca64a3500452ed45b61e79768853e76f
velesov7493 <velesov7493@yandex.ru>
2021-05-22 10:22:45 +0000

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -2,7 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -12,7 +12,7 @@ public class ConvertList2ArrayTest {
     @Test
     public void when7ElementsThen9() {
         int[][] result = ConvertList2Array.toArray(
-                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                List.of(1, 2, 3, 4, 5, 6, 7),
                 3
         );
         int[][] expect = {
--- src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
@@ -13,23 +13,23 @@ public class DepartmentsTest {
 
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenSortAsc() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK2",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -38,7 +38,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2"
         );
-        List<String> expected = Arrays.asList(
+        List<String> expected = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -56,7 +56,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortDesc() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK2",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -65,7 +65,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2"
         );
-        List<String> expected = Arrays.asList(
+        List<String> expected = List.of(
                 "K2",
                 "K2/SK1",
                 "K2/SK1/SSK1",
