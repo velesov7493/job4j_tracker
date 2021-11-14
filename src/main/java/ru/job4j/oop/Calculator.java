cd39942c39b091a4dd85799c8c16c6ea3944d787
Александр Власов <velesov7493@yandex.ru>
2021-11-14 09:01:45 +0300

    1.9. Статические и не статические методы [#504806]

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int y) {
+        return y - x;
+    }
+
+    public double divide(int y) {
+        return (double) y / x;
+    }
+
+    public int multiply(int y) {
+        return x * y;
+    }
+
+    public double sumAllOperations(int y) {
+        return
+                sum(y) + multiply(y)
+                + minus(y) + divide(y);
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println("sum(7) = " + Calculator.sum(7));
+        System.out.println("minus(7) = " + Calculator.minus(7));
+        System.out.println("multiply(7) = " + c.multiply(7));
+        System.out.println("divide(7) = " + c.divide(7));
+        System.out.println("sumAllOperations(7) = " + c.sumAllOperations(7));
+    }
+}
